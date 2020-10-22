2237f359820ff2bed3f5e747b554ca7afbf9421c
joao759 <cocrearesite@gmail.com>
2020-10-22 14:20:14 -0300

    projetoCasaAberta

---
+++ javascript_adaptarvisao.js
@@ -0,0 +1,99 @@
+function alter_ImageNormal() {
+    document.getElementById("img1_Carousel").src = "imagens/img1_Carousel.png";
+    document.getElementById("img2_Carousel").src = "imagens/img2_Carousel.png";
+    document.getElementById("img3_Carousel").src = "imagens/img3_Carousel.png";
+
+    document.getElementById("Protanopia_Olho").src = "imagens/Protanopia_Olho.jpg";
+    document.getElementById("Deuteranopia_Olho").src = "imagens/Deuteranopia_Olho.jpg";
+    document.getElementById("Tritanopia_Olho").src = "imagens/Tritanopia_Olho.jpg";
+
+    document.getElementById("imagem_tipos_Daltonismo").src = "imagens/imagem_tipos_Daltonismo.jpg";
+
+    document.getElementById("verde").src = "imagens/verde_normal.jpg";
+    document.getElementById("azul").src = "imagens/azul_normal.jpg";
+    document.getElementById("vermelho").src = "imagens/vermelho_normal.jpg";
+    document.getElementById("roxo").src = "imagens/roxo_normal.jpg";
+    document.getElementById("laranja").src = "imagens/laranja_normal.jpg";
+    document.getElementById("rosa").src = "imagens/rosa_normal.jpg";
+}
+
+function alter_ImageProtanopia() {
+    document.getElementById("img1_Carousel").src = "imagens/img1_Carousel__AlterFor_Protanopia.png";
+    document.getElementById("img2_Carousel").src = "imagens/img2_Carousel__AlterFor_Protanopia.png";
+    document.getElementById("img3_Carousel").src = "imagens/img3_Carousel__AlterFor_Protanopia.png";
+
+    document.getElementById("imagem_tipos_Daltonismo").src = "imagens/imagem_tipos_Daltonismo__AlterFor_Protanopia.png";
+
+    document.getElementById("Protanopia_Olho").src = "imagens/Protanopia_Olho.jpg";
+    document.getElementById("Deuteranopia_Olho").src = "imagens/Deuteranopia_Olho__AlterFor_Protanopia.png";
+    document.getElementById("Tritanopia_Olho").src = "imagens/Tritanopia_Olho__AlterFor_Protanopia.png";
+
+    document.getElementById("verde").src = "imagens/verde_protanopia.png";
+    document.getElementById("azul").src = "imagens/azul_protanopia.png";
+    document.getElementById("vermelho").src = "imagens/vermelho_protanopia.png";
+    document.getElementById("roxo").src = "imagens/roxo_protanopia.png";
+    document.getElementById("laranja").src = "imagens/laranja_protanopia.png";
+    document.getElementById("rosa").src = "imagens/rosa_protanopia.png";
+}
+
+function alter_ImageDeuteranopia() {
+    document.getElementById("img1_Carousel").src = "imagens/img1_Carousel__AlterFor_Deuteranopia.png";
+    document.getElementById("img2_Carousel").src = "imagens/img2_Carousel__AlterFor_Deuteranopia.png";
+    document.getElementById("img3_Carousel").src = "imagens/img3_Carousel__AlterFor_Deuteranopia.png";
+
+    document.getElementById("imagem_tipos_Daltonismo").src = "imagens/imagem_tipos_Daltonismo__AlterFor_Deuteranopia.png";
+
+    document.getElementById("Protanopia_Olho").src = "imagens/Protanopia_Olho__AlterFor_Deuteranopia.png";
+    document.getElementById("Deuteranopia_Olho").src = "imagens/Deuteranopia_Olho__AlterFor_Deuteranopia.png";
+    document.getElementById("Tritanopia_Olho").src = "imagens/Tritanopia_Olho__AlterFor_Deuteranopia.png";
+
+    document.getElementById("verde").src = "imagens/verde_deuteranopia.png";
+    document.getElementById("azul").src = "imagens/azul_deuteranopia.png";
+    document.getElementById("vermelho").src = "imagens/vermelho_deuteranopia.png";
+    document.getElementById("roxo").src = "imagens/roxo_deuteranopia.png";
+    document.getElementById("laranja").src = "imagens/laranja_deuteranopia.png";
+    document.getElementById("rosa").src = "imagens/rosa_deuteranopia.png";
+}
+
+function alter_ImageTritanopia() {
+    document.getElementById("img1_Carousel").src = "imagens/img3_Carousel.png";
+    document.getElementById("img2_Carousel").src = "imagens/img2_Carousel__AlterFor_Tritanopia.png";
+    document.getElementById("img3_Carousel").src = "imagens/img3_Carousel__AlterFor_Tritanopia.png";
+
+    document.getElementById("imagem_tipos_Daltonismo").src = "imagens/imagem_tipos_Daltonismo__AlterFor_Tritanopia.png";
+
+    document.getElementById("Protanopia_Olho").src = "imagens/Protanopia_Olho__AlterFor_Tritanopia.png";
+    document.getElementById("Deuteranopia_Olho").src = "imagens/Deuteranopia_Olho__AlterFor_Tritanopia.png";
+    document.getElementById("Tritanopia_Olho").src = "imagens/Tritanopia_Olho__AlterFor_Tritanopia.png";
+
+    document.getElementById("verde").src = "imagens/verde_tritanopia.png";
+    document.getElementById("azul").src = "imagens/azul_tritanopia.png";
+    document.getElementById("vermelho").src = "imagens/vermelho_tritanopia.png";
+    document.getElementById("roxo").src = "imagens/roxo_tritanopia.png";
+    document.getElementById("laranja").src = "imagens/laranja_tritanopia.png";
+    document.getElementById("rosa").src = "imagens/rosa_tritanopia.png";
+}
+
+function alter_ImageMonocromia() {
+    document.getElementById("img1_Carousel").src = "imagens/img1_Carousel__AlterFor_Monocromia.png";
+    document.getElementById("img2_Carousel").src = "imagens/img2_Carousel__AlterFor_Monocromia.png";
+    document.getElementById("img3_Carousel").src = "imagens/img3_Carousel__AlterFor_Monocromia.png";
+
+    document.getElementById("imagem_tipos_Daltonismo").src = "imagens/imagem_tipos_Daltonismo__AlterFor_Monocromia.png";
+
+    document.getElementById("Protanopia_Olho").src = "imagens/Olho__AlterFor_Monocromia.png";
+    document.getElementById("Deuteranopia_Olho").src = "imagens/Olho__AlterFor_Monocromia.png";
+    document.getElementById("Tritanopia_Olho").src = "imagens/Olho__AlterFor_Monocromia.png";
+
+    document.getElementById("verde").src = "imagens/verde_monocromia.png";
+    document.getElementById("azul").src = "imagens/azul_monocromia.png";
+    document.getElementById("vermelho").src = "imagens/vermelho_monocromia.png";
+    document.getElementById("roxo").src = "imagens/roxo_monocromia.png";
+    document.getElementById("laranja").src = "imagens/laranja_monocromia.png";
+    document.getElementById("rosa").src = "imagens/rosa_monocromia.png";
+}
+
+$("[data-toggle='toggle']").click(function() {
+    var selector = $(this).data("target");
+    $(selector).toggleClass('in');
+});
